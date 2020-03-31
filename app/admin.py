0df3c02f7f1ebda6a387b0654555903a36c20346
sushenzhang <zhangsushen0@gmail.com>
2020-03-31 19:34:32 -0400

    Check Database

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,9 @@
 
 from django.contrib import admin
 
-from .models import Pic,Material,Color,where_to_use
+from .models import Pic,Material,Color,Object
 
 admin.site.register(Pic)
 admin.site.register(Material)
 admin.site.register(Color)
-admin.site.register(where_to_use)
+admin.site.register(Object)
