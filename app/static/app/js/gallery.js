883e88c71405d7a099eba12587347cc86ffb740c
sushenzhang <zhangsushen0@gmail.com>
2020-04-03 16:01:10 -0400

    fix search function

--- app/static/app/js/gallery.js
+++ app/static/app/js/gallery.js
@@ -2,16 +2,45 @@ filterSelection("all") // Execute the function and show all columns
 function filterSelection(c) {
     var x, i;
     x = document.getElementsByClassName("column");
-    if (c == "all") c = "";
+
     // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
     for (i = 0; i < x.length; i++) {
-        w3RemoveClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+        removeClass(x[i], "show");
+        //if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+        if (isNeedToShow(x[i],c)) addClass(x[i], "show");
     }
 }
+function isNeedToShow(element, c) {
+    if (c == "all") {
+        return true;
+    }
+    var btnContainer = document.getElementById("btnContainer");
+    var btns = btnContainer.getElementsByClassName("btn");
+    var filterList = [];
+    var uniqueFilterList = [];
+    for (var i = 0; i < btns.length; i++) {
+        if (btns[i].classList.contains("active")) {
+            filterList = filterList.concat(btns[i].className.split(" "));
+        }
+    }
+    $.each(filterList, function (i, el) {
+        if ($.inArray(el, uniqueFilterList) === -1) uniqueFilterList.push(el);
+    });
+    uniqueFilterList=uniqueFilterList.filter(function (value, index, arr) {
+        return !(["btn", "active", "btnShowAll", "btnColor"].indexOf(value) > -1)
+    });
+    for (var i = 0; i < uniqueFilterList.length; i++) {
+        if (!(element.className.indexOf(uniqueFilterList[i]) > -1)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 
 // Show filtered elements
-function w3AddClass(element, name) {
+function addClass(element, name) {
     var i, arr1, arr2;
     arr1 = element.className.split(" ");
     arr2 = name.split(" ");
@@ -23,7 +52,7 @@ function w3AddClass(element, name) {
 }
 
 // Hide elements that are not selected
-function w3RemoveClass(element, name) {
+function removeClass(element, name) {
     var i, arr1, arr2;
     arr1 = element.className.split(" ");
     arr2 = name.split(" ");
@@ -36,12 +65,38 @@ function w3RemoveClass(element, name) {
 }
 
 // Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
+
+var btnContainer = document.getElementById("btnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function () {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
+        if (this.classList.contains("active")) {
+            removeClass(this, "active");
+            filterSelection();
+            return;
+        }
+        if (this.classList.contains("btnShowAll")) {
+            var btnContainer = document.getElementById("btnContainer");
+            var btns = btnContainer.getElementsByClassName("btn");
+            for (var i = 0; i < btns.length; i++) {
+                removeClass(btns[i]," active")
+            }
+            this.className += " active";
+            filterSelection("all");
+            return;
+        }
+        removeClass(document.getElementsByClassName("btnShowAll")[0]," active");
+        var current = this.parentElement;
+        current = current.getElementsByClassName("active");
+        try {
+            if (!this.classList.contains("btnColor")) {
+                current[0].className = current[0].className.replace(" active", "");
+
+            } else {
+            }
+        } finally {
+            this.className += " active";
+            filterSelection();
+        }
     });
 }
